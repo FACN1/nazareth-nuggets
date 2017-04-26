14d9a995ff2c405f93eaaa5af1e3dd03cb0f975b
Matt Lubel <matt_lubel@hotmail.com>
2017-04-26 14:58:21 +0300

    prototype initial commit

---
+++ prototype/index.js
@@ -0,0 +1,26 @@
+var addNuggetButton = document.querySelector('.add-nugget-button')
+var map = document.querySelector('.map');
+var mapIcons = document.querySelectorAll('.map-icon');
+var nuggetContainer = document.querySelector('.nugget-container');
+
+// show location select screen when add button is clicked
+addNuggetButton.addEventListener('click', function(e) {
+  var locationSelect = document.querySelector('.location-select')
+  locationSelect.classList.toggle('hidden');
+  map.classList.toggle('faded');
+})
+
+// bring nugget info onto screen when a map icon is clicked
+mapIcons.forEach(function(icon) {
+  icon.addEventListener('click', function(e) {
+    nuggetContainer.classList.toggle('not-visible');
+    nuggetContainer.classList.toggle('visible');
+    event.stopPropagation();
+  })
+})
+
+// remove nugget info from screen when anywhere is clicked
+document.addEventListener('click', function(e) {
+  nuggetContainer.classList.add('not-visible');
+  nuggetContainer.classList.remove('visible');
+})
