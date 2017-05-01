069a54b0217ec40c14c1ac9ca5bc30742666cd89
suha-edris <suha.edris@gmail.com>
2017-05-01 19:18:07 +0300

    display Nazareth mmap on homepage #21

---
+++ app/database/db_build.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+DROP TABLE IF EXISTS nuggets;
+
+CREATE TABLE nuggets(
+  id SERIAL PRIMARY KEY,
+  lat DOUBLE PRECISION NOT NULL,
+  long DOUBLE PRECISION NOT NULL,
+  category VARCHAR(30) NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  description TEXT,
+  img_url VARCHAR(100),
+  author VARCHAR(30) NOT NULL
+)
---
+++ app/public/map.js
@@ -0,0 +1,13 @@
+var bounds = L.latLngBounds([32.683154, 35.278158], [32.723174, 35.341721])
+var mymap = L.map('mapid', {
+  center: [32.699, 35.303],
+  zoom: 13,
+  maxBounds: bounds
+})
+
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3V1dXVoYSIsImEiOiJjajI2N3QwZm0wMDE1MnFwb3NnYnhwaG55In0.sZgdmc9B4GG9X4Bx5o3NWg', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.light',
+  accessToken: 'pk.eyJ1Ijoia2FyeXVtIiwiYSI6ImNqMjAzNGU4ZjAxa3EycW4xazFxcHZ6a2QifQ.m_dNO1l1sMkM7r4d5nlRRQ'
+}).addTo(mymap)
