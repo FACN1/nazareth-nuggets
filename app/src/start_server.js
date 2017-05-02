dadf0ecc3e044aa9cf2ede8fd9f7bdaf2036f271
suha-edris <suha.edris@gmail.com>
2017-05-02 16:16:24 +0300

    change port to PORT

--- app/src/start_server.js
+++ app/src/start_server.js
@@ -1,6 +1,6 @@
 const app = require('../src/server.js')
 
-const port = process.env.port || 8111
+const port = process.env.PORT || 8111
 
 app.listen(port, function () {
   console.log(`The magic happens on port ${port}!`)
