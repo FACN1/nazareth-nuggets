d15d94373ddb0ec22ac7e9d74d1ec9c1f1a11a0f
suha-edris <suha.edris@gmail.com>
2017-05-02 11:30:33 +0300

    add minZoom to the map and resolve requested changes

--- app/public/map.js
+++ app/public/map.js
@@ -1,15 +1,15 @@
 /* global L */
 
 var bounds = L.latLngBounds([32.683154, 35.278158], [32.723174, 35.341721])
-var mymap = L.map('mapid', {
+var mymap = L.map('map', {
   center: [32.699, 35.303],
   zoom: 13,
-  maxBounds: bounds
+  maxBounds: bounds,
+  minZoom: 12
 })
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3V1dXVoYSIsImEiOiJjajI2N3QwZm0wMDE1MnFwb3NnYnhwaG55In0.sZgdmc9B4GG9X4Bx5o3NWg', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
   id: 'mapbox.light',
   accessToken: 'pk.eyJ1Ijoia2FyeXVtIiwiYSI6ImNqMjAzNGU4ZjAxa3EycW4xazFxcHZ6a2QifQ.m_dNO1l1sMkM7r4d5nlRRQ'
 }).addTo(mymap)
