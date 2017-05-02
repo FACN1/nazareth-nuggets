24f0e24eaad3e3c9bb7971c397a8d408a171adba
Mario <mario.saliba98@gmail.com>
2017-05-02 15:27:11 +0300

    #26 adds a marker for the user's location

--- app/public/map.js
+++ app/public/map.js
@@ -5,7 +5,7 @@
     center: [32.699, 35.303],
     zoom: 13,
     maxBounds: bounds,
-    minZoom: 12
+    minZoom: 13
   })
 
   L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3V1dXVoYSIsImEiOiJjajI2N3QwZm0wMDE1MnFwb3NnYnhwaG55In0.sZgdmc9B4GG9X4Bx5o3NWg', {
@@ -13,4 +13,21 @@
     id: 'mapbox.light',
     accessToken: 'pk.eyJ1Ijoia2FyeXVtIiwiYSI6ImNqMjAzNGU4ZjAxa3EycW4xazFxcHZ6a2QifQ.m_dNO1l1sMkM7r4d5nlRRQ'
   }).addTo(mymap)
+
+  mymap.locate({setView: true, maxZoom: 20})
+
+  function onLocationFound (e) {
+    var radius = e.accuracy / 2
+
+    L.marker(e.latlng).addTo(mymap)
+
+    L.circle(e.latlng, radius).addTo(mymap)
+  }
+
+  function onLocationError (e) {
+    window.alert(e.message)
+  }
+
+  mymap.on('locationerror', onLocationError)
+  mymap.on('locationfound', onLocationFound)
 })()
