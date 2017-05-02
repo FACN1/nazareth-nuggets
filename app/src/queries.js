ace953779e612cf1c4abb9769f5b588d70214443
hilbertspitzer <hilbertbonamy@gmail.com>
2017-05-02 12:04:26 +0300

    #33 creates staticfilepath

--- app/src/queries.js
+++ app/src/queries.js
@@ -1,7 +1,12 @@
 const dbConnection = require('../database/db_connection.js')
 
 const getAll = (cb) => {
-  dbConnection.query('SELECT * FROM nuggets', cb)
+  dbConnection.query('SELECT * FROM nuggets', (error, result) => {
+    if (error) {
+      return cb(error)
+    }
+    return cb(null, result)
+  })
 }
 
 module.exports = {
--- app/src/server.js
+++ app/src/server.js
@@ -1,23 +1,19 @@
-// const path = require('path')
+const path = require('path')
 const express = require('express')
 const query = require('./queries.js')
 const app = express()
 
-// const staticFilesPath = path.join(__dirname, '../public')
-// app.use(express.static(staticFilesPath))
+const staticFilesPath = path.join(__dirname, '../public')
+app.use(express.static(staticFilesPath))
 
-// a// const staticFilesPath = app.use((path.join(__dirname, '../public')))
-// a// app.use(express.static(staticFilesPath))
-
-app.get('/', function (req, res) {
+app.get('/all-nuggets', function (req, res) {
   query.getAll((err, result) => {
     if (err) {
       console.log(err)
       return
     }
-    console.log('result is ', result)
+    console.log('result is ', result.rows)
   })
-  res.send('jufufuy')
 })
 
 app.listen(4000, function () {
