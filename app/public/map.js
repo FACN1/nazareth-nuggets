8ca1f42d5c9f5b2e73deb56fbfa38b3699f1be0e
Matt Lubel <matt_lubel@hotmail.com>
2017-05-07 16:25:04 +0300

    #43 implement on zoomend function to select which icons to display

--- app/public/map.js
+++ app/public/map.js
@@ -2,8 +2,16 @@
 
 var nazarethNuggets = (function () { // eslint-disable-line
 
-  function generateMockNuggets (quantity, categories, latBounds, lngBounds) {
+  // bounds for leaflet in format: [south-west, north-east]
+  var nazarethBounds = [
+    [32.683154, 35.278158],
+    [32.723174, 35.341721]
+  ]
+
+  function generateMockNuggets (quantity, categories, bounds) {
     var nuggets = []
+    var latBounds = [bounds[0][0], bounds[1][0]]
+    var lngBounds = [bounds[0][1], bounds[1][1]]
     for (var i = 0; i < quantity; i++) {
       nuggets.push({
         id: i,
@@ -30,37 +38,55 @@ var nazarethNuggets = (function () { // eslint-disable-line
     xhr.send()
   }
 
-  var icons = {
+  var bigIconsMap = {
     food: L.icon({
       iconUrl: './assets/food.png',
       iconSize: [24, 24]
     }),
     nature: L.icon({
-      iconUrl: './assets/leaf.png',
+      iconUrl: './assets/nature.png',
+      iconSize: [24, 24]
+    }),
+    exclamation: L.icon({
+      iconUrl: './assets/exclamation.png',
       iconSize: [24, 24]
     })
   }
 
-  function addIconsToMap (nuggets) {
-  // nuggets is an array of objects which holds the data from the db
-    nuggets.forEach(function (nugget) {
-      // for each nugget we want to make a marker and put it on the map
+  var smallIconsMap = {
+    food: L.icon({
+      iconUrl: './assets/food-small.png',
+      iconSize: [8, 8]
+    }),
+    nature: L.icon({
+      iconUrl: './assets/nature-small.png',
+      iconSize: [8, 8]
+    }),
+    exclamation: L.icon({
+      iconUrl: './assets/exclamation-small.png',
+      iconSize: [8, 8]
+    })
+  }
 
-      L.marker([nugget.lat, nugget.long], {
+  function createIconsLayer (nuggets, iconsMap) {
+  // nuggets is an array of objects which holds the data from the db
+    var icons = nuggets.map(function (nugget) {
+      // for each nugget we want to make a marker and put it on the map
+      return L.marker([nugget.lat, nugget.long], {
         id: nugget.id,
         category: nugget.category,
         title: nugget.title,
         description: nugget.description,
         img_url: nugget.img_url,
         author: nugget.author,
-        icon: icons[nugget.category]
+        icon: iconsMap[nugget.category]
       })
       .on('click', function (e) {
         // to be implemented
         console.log(e.target.options)
       })
-      .addTo(mymap)
     })
+    return L.layerGroup(icons)
   }
 
   var mymap = L.map('map', {
@@ -68,11 +94,7 @@ var nazarethNuggets = (function () { // eslint-disable-line
     zoomControl: false,
     attributionControl: false,
     zoom: 15,
-    maxBounds: [
-      // bounds for nazareth
-      [32.683154, 35.278158],
-      [32.723174, 35.341721]
-    ],
+    maxBounds: nazarethBounds,
     minZoom: 13
   })
 
@@ -93,13 +115,10 @@ var nazarethNuggets = (function () { // eslint-disable-line
   mymap.on('locationfound', onLocationFound)
 
   var nuggets = generateMockNuggets(
-    100,
-    ['food', 'nature'],
-    [32.683154, 32.723174],
-    [35.278158, 35.341721]
+    50,
+    ['food', 'nature', 'exclamation'],
+    nazarethBounds
   )
-
-  console.log(nuggets)
 
   requestNuggets('GET', '/all-nuggets', function (err, res) {
     if (err) {
@@ -108,16 +127,23 @@ var nazarethNuggets = (function () { // eslint-disable-line
     console.log(res)
   })
 
-  var zoomLevel = 13
+  var displayCorrectIcons = function (e) {
+    // small icons if zoomed less than level 15, big icons otherwise
+    if (mymap.getZoom() < 15) {
+      smallIconsLayer.addTo(mymap)
+      bigIconsLayer.removeFrom(mymap)
+    } else {
+      smallIconsLayer.removeFrom(mymap)
+      bigIconsLayer.addTo(mymap)
+    }
+  }
 
-  mymap.on('zoomstart', function () {
-    console.log('zoomstart event')
+  // on zoomend is good but not perfect, because can zoom multiple levels before this function will re-run
+  mymap.on('zoomend', displayCorrectIcons)
 
-    zoomLevel = mymap.getZoom()
-    console.log('zoom variable: ' + zoomLevel)
-  })
-
-  addIconsToMap(nuggets)
+  var smallIconsLayer = createIconsLayer(nuggets, smallIconsMap)
+  var bigIconsLayer = createIconsLayer(nuggets, bigIconsMap)
+  smallIconsLayer.addTo(mymap)
 
   var centerButton = document.querySelector('.center-button')
   centerButton.addEventListener('click', function (e) {
